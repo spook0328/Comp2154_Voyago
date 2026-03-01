ac8f10f6a00fd281c8c7aedbb5483d8c72045f90
Rojan Jafarnezhad <rojanjafarnezhad@gmail.com>
2026-03-01 18:02:56 -0500

    Add Itineraries DbSet to ApplicationDbContext

--- TripPlanner/TripPlanner/Data/ApplicationDbContext.cs
+++ TripPlanner/TripPlanner/Data/ApplicationDbContext.cs
@@ -9,4 +9,7 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    // tells EF Core to create an Itineraries table based on the Itinerary model
+    public DbSet<Itinerary> Itineraries { get; set; }
 }
