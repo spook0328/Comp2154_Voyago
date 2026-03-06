20368e1f9b9afa084ce694e7cc42595259426d0e
Cheng Yeh Tsai <181903030+Mason0167@users.noreply.github.com>
2026-03-06 14:07:09 -0500

    Minor adjustments

--- TripPlanner/TripPlanner/Models/ApplicationUser.cs
+++ TripPlanner/TripPlanner/Models/ApplicationUser.cs
@@ -8,10 +8,5 @@ namespace TripPlanner.Models;
 
 public class ApplicationUser: IdentityUser
 {
-    // Need to discuss what will be stored
-    public byte[]? ProfilePicture { get; set; } 
-    public string? ProfilePictureMimeType { get; set; } 
-    public DateTime? LastPhoneNumberChangeDate { get; set; } 
-    public int PhoneNumberChangeCount { get; set; } = 0;
     public ICollection<Itinerary> Itineraries { get; set; }
 }
